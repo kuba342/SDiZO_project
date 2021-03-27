5ac150dbb3b5a41fb41277e413f8ea0c7bc34d10
https://github.com/kuba342 <j.derda010@gmail.com>
2021-03-27 14:01:34 +0100

    Lista dwukierunkowa - chyba ok

--- App/BidirectionalListLib/BiList.h
+++ App/BidirectionalListLib/BiList.h
@@ -22,13 +22,14 @@ public:
     //METODY OPERUJĄCE NA LIŚCIE
     void addAtTheBeginning(int number);
     void addAtTheEnd(int number);
-    void addOnPosition(int index, int number);
+    void addOnPosition(listElement* element, int number);
     void removeAtTheBeginning();
     void removeAtTheEnd();
-    void removeOnPosition();
+    void removeOnPosition(listElement* element);
     void showList();
 
     //SETTERY I GETTERY
+    listElement* getElement(int n);
     int getCount();
     listElement* getHead();
     listElement* getTail();
@@ -53,49 +54,139 @@ BiList::~BiList()
 /*********METODY OPERUJĄCE NA LIŚCIE**********/
 void BiList::addAtTheBeginning(int number){
     listElement* pointer;
-    
+    //Nowy element
     pointer = new listElement;
+    //Wpisuje dane do nowego elementu
     pointer->data = number;
+    //Nowy element nie ma poprzednika
     pointer->previous = nullptr;
+    //W pole next wpisuję adres czoła listy
     pointer->next = this->head;
-
+    //W polu head listy umieszczam adres nowego elementu
     this->head = pointer;
+    //Zwiększam licznik elementów o 1
     this->count++;
-
+    //Sprawdzam czy istnieje następnik czoła listy i w jego pole previous wpisuję
+    //adres dodawanego elementu
     if(pointer->next){
-        pointer->next = pointer;
-        pointer->previous = pointer;
+        pointer->next->previous = pointer;
     }
     else{
-        this->tail = pointer;
+        this->tail = pointer;   //Jeśli nie ma następnika, to nowy element jest także ogonem.
     }
 }
 
 void BiList::addAtTheEnd(int number){
-
+    listElement* pointer;
+    //Nowy element
+    pointer = new listElement;
+    //Dane dla nowego elementu
+    pointer->data = number;
+    //W pole next adres zerowy, bo nie ma następnika
+    pointer->next = nullptr;
+    //W pole previous zawartość pola tail - poprzednik tworzonego elementu
+    pointer->previous = this->tail;
+    //Do pola tail wpisuję adres tworzonego elementu
+    this->tail = pointer;
+    //Zwiększam licznik o 1
+    this->count++;
+    //Sprawdzam czy istnieje poprzednik, jeśli nie, wpisuję do czoła nowy element
+    if(pointer->previous){
+        pointer->previous->next = pointer;
+    }
+    else{
+        this->head = pointer;
+    }
 }
 
-void BiList::addOnPosition(int index, int number){
+//Dodaj element przed wybranym
+void BiList::addOnPosition(listElement* element, int number){
+    listElement* pointer;
+    //Jeśli element jest czołem, dodaj na początek listy
+    if(element == this->head){
+        this->addAtTheBeginning(number);
+    }
+    else{
+        pointer = new listElement;
+        //Wpisuję dane
+        pointer->data = number;
+        //Następnik to element podany
+        pointer->next = element;
+        //Poprzednikiem jest poprzednik wskazanego elementu
+        pointer->previous = element->previous;
 
+        this->count++;
+
+        //ustawiam w poprzedniku następnika jako nowy element
+        element->previous->next = pointer;
+        //Ustawiam w następniku poprzednik - nowy element
+        element->previous = pointer;
+    }
 }
 
 void BiList::removeAtTheBeginning(){
-
+    if(this->count){
+        removeOnPosition(this->head);
+    }
 }
 
 void BiList::removeAtTheEnd(){
-
+    if(this->count){
+        removeOnPosition(this->tail);
+    }
 }
 
-void BiList::removeOnPosition(){
+void BiList::removeOnPosition(listElement* element){
+    //Zmniejszam licznik listy
+    this->count--;
+    
+    //Jeśli istnieje poprzednik, to jako następnik przypisuję mu 
+    //następnika usuwanego elementu
+    if(element->previous){
+        element->previous->next = element->next;
+    }
+    else{
+        this->head = element->next;     //W przeciwnym razie do czoła przypisuję następnik
+    }
 
+    //Jeśli element posiada następnik, to w jego pole poprzednika wpisuję poprzednika
+    //usuwanego elementu. W przypadku ogona wpisuję poprzednik usuwanego elementu
+    if(element->next){
+        element->next->previous = element->previous;
+    }
+    else{
+        this->tail = element->previous;
+    }
+
+    delete element;
 }
 
+//ZROBIĆ ZABEZPIECZENIE PRZED PUSTĄ LISTĄ W OPERACJACH!
 void BiList::showList(){
+    listElement* pointer;
+    pointer = this->head;
 
+    std::cout << "List: [";
+    for(pointer; pointer!=nullptr; pointer=pointer->next){
+        std::cout << " " << pointer->data << " ";
+    }
+    std::cout << "]";
 }
 
 /*********SETTERY I GETTERY*******************/
+listElement* BiList::getElement(int n){
+    listElement* pointer = this->head;
+    if(n<=this->count){
+        for(int i=0; i<n; i++){
+            pointer = pointer->next;
+        }
+        return pointer;
+    }
+    else{
+        return nullptr;
+    }
+}
+
 int BiList::getCount(){
     return this->count;
 }
