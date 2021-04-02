ecef2953a0ed0a9058b3f6b02f1498e3ac661f52
https://github.com/kuba342 <j.derda010@gmail.com>
2021-04-02 22:35:18 +0200

    początek drzewa BR

--- App/BRTree/BRTree.h
+++ App/BRTree/BRTree.h
@@ -1,16 +1,35 @@
 #pragma once
 
+struct Node
+{
+    Node* up;
+    Node* left;
+    Node* right;
+    int key;
+    char color;
+};
+
+
 class BRTree
 {
 private:
-    /* data */
+    Node* guardian;
+    Node* root;
+
+
 public:
-    BRTree(/* args */);
+    BRTree();
     ~BRTree();
 };
 
-BRTree::BRTree(/* args */)
-{
+BRTree::BRTree(){
+    //Inicjalizacja strażnika
+    this->guardian->color = 'B';
+    this->guardian->up = this->guardian;
+    this->guardian->left = this->guardian;
+    this->guardian->right = this->guardian;
+    //Korzeń wskazuje na strażnika
+    this->root = this->guardian;
 }
 
 BRTree::~BRTree()
